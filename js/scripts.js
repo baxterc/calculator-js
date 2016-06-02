e0bcbc0fe7ecffb7225e78b8a694633a5317a30f
Charlie Baxter and Caleb Stokka <charlie@>
2016-06-02 15:17:59 -0700

    changed jquery to branching logic one set of forms and radio button selectors

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//business logic code
+
 var add = function(number1,  number2){
 return number1 + number2;
 };
@@ -14,51 +16,24 @@ var divide = function(number1,  number2){
 return number1 / number2;
 };
 
+//user interface code
+
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("form#calculator").submit(function() {
     event.preventDefault();
-    var number1 = parseInt($("#add .one").val());
-    var number2 = parseInt($("#add .two").val());
-    var result = add(number1, number2);
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
     $("#output").text(result);
-  });
-
-  $("form#subtract").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#subtract .one").val());
-    var number2 = parseInt($("#subtract .two").val());
-    var result = subtract(number1, number2);
-    $("#output").text(result);
-  });
-
-  $("form#multiply").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#multiply .one").val());
-    var number2 = parseInt($("#multiply .two").val());
-    var result = multiply(number1, number2);
-    $("#output").text(result);
-  });
-
-  $("form#divide").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#divide .one").val());
-    var number2 = parseInt($("#divide .two").val());
-    var result = divide(number1, number2);
-    $("#output").text(result);
-  });
+  });  
 });
-
-  // var zero = 0;
-  // var one = 1;
-  // var two = 2;
-  // var three = 3;
-  // var four = 4;
-  // var five = 5;
-  // var six = 6;
-  // var seven = 7;
-  // var eight = 8;
-  // var nine = 9;
-
-  //   $(".number").click(function() {
-  //     $("#result").append(this);
-  //   })
